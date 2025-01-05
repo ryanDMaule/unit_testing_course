485ed05760555d6cccae670cc24054a7ec67bafb
ryanDMaule <davidmaule98@gmail.com>
2025-01-05 20:04:55 +0000

    Test double exercise

--- test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/exercise4/FetchUserProfileUseCaseSync.java
+++ test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/exercise4/FetchUserProfileUseCaseSync.java
@@ -17,8 +17,7 @@ public class FetchUserProfileUseCaseSync {
     private final UserProfileHttpEndpointSync mUserProfileHttpEndpointSync;
     private final UsersCache mUsersCache;
 
-    public FetchUserProfileUseCaseSync(UserProfileHttpEndpointSync userProfileHttpEndpointSync,
-                                       UsersCache usersCache) {
+    public FetchUserProfileUseCaseSync(UserProfileHttpEndpointSync userProfileHttpEndpointSync, UsersCache usersCache) {
         mUserProfileHttpEndpointSync = userProfileHttpEndpointSync;
         mUsersCache = usersCache;
     }
@@ -26,23 +25,16 @@ public class FetchUserProfileUseCaseSync {
     public UseCaseResult fetchUserProfileSync(String userId) {
         EndpointResult endpointResult;
         try {
-            // the bug here is that userId is not passed to endpoint
-            endpointResult = mUserProfileHttpEndpointSync.getUserProfile("");
-            // the bug here is that I don't check for successful result and it's also a duplication
-            // of the call later in this method
-            mUsersCache.cacheUser(
-                    new User(userId, endpointResult.getFullName(), endpointResult.getImageUrl()));
+            endpointResult = mUserProfileHttpEndpointSync.getUserProfile(userId);
+            if (isSuccessfulEndpointResult(endpointResult)) {
+                mUsersCache.cacheUser(new User(userId, endpointResult.getFullName(), endpointResult.getImageUrl()));
+                return UseCaseResult.SUCCESS;
+            }
         } catch (NetworkErrorException e) {
             return UseCaseResult.NETWORK_ERROR;
         }
 
-        if (isSuccessfulEndpointResult(endpointResult)) {
-            mUsersCache.cacheUser(
-                    new User(userId, endpointResult.getFullName(), endpointResult.getImageUrl()));
-        }
-
-        // the bug here is that I return wrong result in case of an unsuccessful server response
-        return UseCaseResult.SUCCESS;
+        return UseCaseResult.FAILURE;
     }
 
     private boolean isSuccessfulEndpointResult(EndpointResult endpointResult) {
